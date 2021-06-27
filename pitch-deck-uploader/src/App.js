74dd9c0a2bf18bd0bab2f12d9de305a833888010
Adam Kalimi <adam.kalimi@yahoo.com>
2021-06-26 20:51:38 -0400

    Imported pdftron into projcet. Built initial webviewer component.

--- pitch-deck-uploader/src/App.js
+++ pitch-deck-uploader/src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useRef, useEffect} from 'react';
+import WebViewer from '@pdftron/webviewer';
 
 function App() {
+  const viewer = useRef(null);
+  const file = 
+
+  useEffect(() => {
+
+    WebViewer(
+      { 
+        path: '../lib', 
+        pdftronServer: 'https://demo.pdftron.com/',
+        initialDoc: '../files/01 - application letter.pdf',
+        // disabledElements: [
+        //   'viewControlsButton',
+        //   'viewControlsOverlay'
+        // ]
+     }, 
+     viewer.current,
+    ).then(instance => {
+        const { docViewer } = instance;
+        var Feature = instance.Feature;
+        instance.disableFeatures([Feature.header]);
+        instance.disableFeatures([Feature.Copy]);
+        instance.loadDocument('../files/01 - application letter.pdf');
+     })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="app">
+      <div className="webviewer" ref={viewer} style={{height: "100vh"}}></div>
     </div>
   );
 }
